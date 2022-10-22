1aa018a0203e71c1c462e1720f29e93af889b467
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-22 20:38:12 +0530

    comment added

--- Main.js
+++ Main.js
@@ -1,10 +1,12 @@
 const puppeteer = require("puppeteer");
 const loginLink = "https://www.hackerrank.com/auth/login";
+// temprory email 
 let email = "mefiv72384@abudat.com";
+// 
 const password = "devesh123";
 let page;
 console.log("before");
-
+// puppeteer launch returns the chromium browser open promise 
 let browserWillbeLauncedPromise = puppeteer.launch({
   headless: false,
   defaultViewport: null,
@@ -19,30 +21,36 @@ browserWillbeLauncedPromise
     return newTabPromise;
   })
   .then(function (newTabPromise) {
-    page = newTabPromise;
+    // inside the page newTabPromise instance added for further  use
+    page = newTabPromise; // hackerrank tab is inside of newTabPromise
     let pageWillbeOpenedPromise = newTabPromise.goto(loginLink);
     return pageWillbeOpenedPromise;
   })
   .then(function () {
-    let emailWillBeTypedPromise = page.type('input[id="input-1"]', email, {
-      delay: 10,
+    // email typeing promise used using page instance becuase inside of page hackerrank tab insatnce saved . 
+    let emailWillBeTypedPromise = page.type('input[id="input-1"]', email, { // input area selected using attriubute selector 
+      delay: 100, // dealy for latter type
     });
 
     return emailWillBeTypedPromise;
   })
+  
   .then(() => {
+    // password typed with page instance . using attribute selector
     let passwordWillBeTypedPromise = page.type(
       "input[id ='input-2']",
       password,
-      { delay: 10 }
+      { delay: 100 }
     );
     return passwordWillBeTypedPromise;
   })
   .then(() => {
+    // rember me marker clicked persented in page tab
     let rememberMeWillBeClickPromise = page.click(".checkbox-input");
     return rememberMeWillBeClickPromise;
   })
   .then(() => {
+    // log in button clicked using page instance and attribute selector
     let loginButtonWillClicked = page.click(
       'button[data-analytics="LoginPassword"]'
     );
