a649b41f25265d728f85a8e43ab5d2fd34e5b14e
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-22 19:50:05 +0530

    added

--- Main.js
+++ Main.js
@@ -1,15 +1,35 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+const loginLink = "https://www.hackerrank.com/auth/login";
+let email = "mefiv72384@abudat.com";
+const password = "devesh123";
+let page;
+console.log("before");
 
-let browserOpenPromise = puppeteer.launch({
-    headless : false,
-    args : ['--start-minimization'],
-    defaultViewport : null
-})
+let browserWillbeLauncedPromise = puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+  args: ["--start-maximized"],
+});
 
-browserOpenPromise.then((brwoserInstance) =>{
- let newTabPromise= brwoserInstance.newPage();
- return newTabPromise
-}).then((newTabPromise)=>{
-    let hackerrankPagePromise = newTabPromise.goto("https://www.hackerrank.com/");
+browserWillbeLauncedPromise
+  .then(function (brwoserInstance) {
+    let newTabPromise = brwoserInstance.newPage();
+     
+    // newTabPromise instance added into page  for further use
+    return newTabPromise;
+  })
+  .then(function (newTabPromise) {
+     page = newTabPromise;
+    let pageWillbeOpenedPromise = newTabPromise.goto(loginLink);
+    return pageWillbeOpenedPromise;
+  })
+  .then(function () {
+   let emailWillBeTypedPromise = page.type('input[id="input-1"]' , email , {delay : 200})
 
-})
+   return emailWillBeTypedPromise; 
+  }).then(()=>{
+    let passwordWillBeTypedPromise = page.type("input[id ='input-2']" , password , {delay : 150});
+    return passwordWillBeTypedPromise
+  })
+
+  console.log("after");
