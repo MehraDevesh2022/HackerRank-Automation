00974939edbfcb6d88708584aa3c2953ff94b4d9
MehraDevesh2022 <Mehra.Devesh2022@gmail.com>
2022-10-26 18:31:38 +0530

    async await hackerrank project done

--- hk.js
+++ hk.js
@@ -24,11 +24,9 @@ console.log("before");
     await waitAndClick('.topic-card>a[data-attr1="algorithms"]' , newTabPromise);
     await waitAndClick('input[value="warmup"]' ,newTabPromise );
     let allQuestionArrayPromise =await newTabPromise.$$(".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled");
-    console.log(allQuestionArrayPromise.length);
-     
-    await questionSolver(newTabPromise , allQuestionArrayPromise[0] , code[0]);
+    await questionSolver(newTabPromise , allQuestionArrayPromise[0] , codeArr.answers[0]);
 
-
+ 
 
 
 })()
@@ -43,56 +41,26 @@ async function waitAndClick(selector, currentPage) {
    
     };
 
+// error coming from question solver function
 
-function questionSolver(Page, question, answer) {
-    return new Promise(function (resolve, reject) {
-        let questionWillClickedPromise = question.click();
-        questionWillClickedPromise.then(function () {
-            let customInputBoxClickedPromise = waitAndClick(".checkBoxWrapper .ui-checkbox.theme-m .label-wrap .checkbox-wrap", Page);
-            return customInputBoxClickedPromise;
-        }).then(function () {
-            return page.type(".input.text-area.custominput.auto-width", answer, { delay: 10 })
-        }).then(function () {
-            let ctrlIsPressedPromise = page.keyboard.down("Control");
-            return ctrlIsPressedPromise;
-        }).then(function () {
-            let AisPressedPromise = page.keyboard.press("A", { delay: 40 });
-            return AisPressedPromise;
-        }).then(function () {
-            let XisPressedPromise = page.keyboard.press("X");
-            return XisPressedPromise;
-        }).then(function () {
-            let ctrlKeyUpPromoise = page.keyboard.up("Control");
-            return ctrlKeyUpPromoise;
-        }).then(function () {
-            let eidtorSectionClickedPromise = waitAndClick(
-                ".hr-monaco-base-editor.showUnused .monaco-editor.no-user-select.vs", page);
-            return eidtorSectionClickedPromise;
-        }).then(function () {
-            let ctrlIsPressedPromise = page.keyboard.down("Control");
-            return ctrlIsPressedPromise;
-        }).then(function () {
-            let AisPressedPromise = page.keyboard.press("A", { delay: 40 });
-            return AisPressedPromise;
-        }).then(function () {
-            return page.keyboard.press('V', { delay: 100 })
-
-        })
-            .then(function () {
-                return page.click('.hr-monaco__run-code');
-            }).then(function () {
-                return page.waitForSelector('.testcase-item');
-            }).then(function () {
-                return page.click('.hr-monaco-submit', { delay: 1000 });
-            }).then(function () {
-                return page.waitForSelector('.submission-wrapper-next-entity-btn');
-            }).then(function () {
-                resolve();
-            }).catch(function (err) {
-                console.log(err);
-            })
-
-    })
+async function questionSolver(page, question, answer) {
+   await question.click();
+    await waitAndClick(".checkBoxWrapper .ui-checkbox.theme-m .label-wrap .checkbox-wrap", page);
+    await page.type(".input.text-area.custominput.auto-width", answer, { delay: 10 })
+    await page.keyboard.down("Control");
+    await page.keyboard.press("A", { delay: 40 });
+    await page.keyboard.press("X");
+    await page.keyboard.up("Control");
+    await waitAndClick(".hr-monaco-base-editor.showUnused .monaco-editor.no-user-select.vs", page);
+    await page.keyboard.down("Control");
+    await page.keyboard.press("A", { delay: 40 });
+    await page.keyboard.press('V', { delay: 100 })
+    await page.click('.hr-monaco__run-code');
+    await page.waitForSelector('.testcase-item');
+    await page.click('.hr-monaco-submit', { delay: 1000 });
+    await waitAndClick('.ui-btn.ui-btn-normal.ui-btn-secondary.submission-wrapper-next-entity-btn.ui-btn-link.ui-btn-styled' , page);
+    // await page.clkick('.align-left-conatiner .community-breadcrumb.text-content a[data-attr1="Warmup"]', {delay :50});
+ 
 }
 
 console.log("after");
